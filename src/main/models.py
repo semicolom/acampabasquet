3a9076bf4f9f24bfbe9ac2ba0e3de593af6ac154
Toni Colom <toni@semicolom.com>
2019-05-25 11:49:51 +0200

    Adds match generator base file

--- src/main/models.py
+++ src/main/models.py
@@ -13,9 +13,20 @@ SEXES = [
 
 
 class Group(models.Model):
-    name = models.CharField("Nom", max_length=255)
-    category = models.CharField("Categoria", choices=AGES, max_length=255)
-    modality = models.CharField("Modalitat", choices=SEXES, max_length=255)
+    name = models.CharField(
+        "Nom",
+        max_length=255,
+    )
+    category = models.CharField(
+        "Categoria",
+        choices=AGES,
+        max_length=255,
+    )
+    modality = models.CharField(
+        "Modalitat",
+        choices=SEXES,
+        max_length=255,
+    )
 
     class Meta:
         verbose_name = "Grup"
@@ -25,10 +36,21 @@ class Group(models.Model):
 
 
 class Team(models.Model):
-    name = models.CharField("Nom", max_length=255)
+    name = models.CharField(
+        "Nom",
+        max_length=255,
+    )
 
-    category = models.CharField("Categoria", choices=AGES, max_length=255)
-    modality = models.CharField("Modalitat", choices=SEXES, max_length=255)
+    category = models.CharField(
+        "Categoria",
+        choices=AGES,
+        max_length=255,
+    )
+    modality = models.CharField(
+        "Modalitat",
+        choices=SEXES,
+        max_length=255,
+    )
 
     group = models.ForeignKey(
         Group,
@@ -38,11 +60,26 @@ class Team(models.Model):
         on_delete=models.SET_NULL,
     )
 
-    points = models.PositiveIntegerField("Punts a favor", default=0)
-    points_against = models.PositiveIntegerField("Punts en contra", default=0)
-    games_played = models.PositiveIntegerField("Partits jugats", default=0)
-    games_won = models.PositiveIntegerField("Partits guanyats", default=0)
-    games_lost = models.PositiveIntegerField("Partits perduts", default=0)
+    points = models.PositiveIntegerField(
+        "Punts a favor",
+        default=0,
+    )
+    points_against = models.PositiveIntegerField(
+        "Punts en contra",
+        default=0,
+    )
+    games_played = models.PositiveIntegerField(
+        "Partits jugats",
+        default=0,
+    )
+    games_won = models.PositiveIntegerField(
+        "Partits guanyats",
+        default=0,
+    )
+    games_lost = models.PositiveIntegerField(
+        "Partits perduts",
+        default=0,
+    )
 
     class Meta:
         verbose_name = "Equip"
@@ -52,8 +89,14 @@ class Team(models.Model):
 
 
 class Field(models.Model):
-    name = models.CharField("Nom", max_length=255)
-    for_finals = models.BooleanField("Per jugar finals", default=False)
+    name = models.CharField(
+        "Nom",
+        max_length=255,
+    )
+    for_finals = models.BooleanField(
+        "Per jugar finals",
+        default=False,
+    )
 
     class Meta:
         verbose_name = "Pista"
@@ -64,7 +107,10 @@ class Field(models.Model):
 
 
 class Match(models.Model):
-    name = models.CharField("Nom", max_length=255)
+    name = models.CharField(
+        "Nom",
+        max_length=255,
+    )
 
     home_team = models.ForeignKey(
         Team,
@@ -79,11 +125,21 @@ class Match(models.Model):
         related_name='games_as_away_team',
     )
 
-    home_team_points = models.PositiveIntegerField("Punts equip local", default=0)
-    away_team_points = models.PositiveIntegerField("Punts equip visitant", default=0)
+    home_team_points = models.PositiveIntegerField(
+        "Punts equip local",
+        default=0,
+    )
+    away_team_points = models.PositiveIntegerField(
+        "Punts equip visitant",
+        default=0,
+    )
 
     start_time = models.DateTimeField("Hora de joc")
-    game_field = models.ForeignKey(Field, verbose_name="Pista", on_delete=models.PROTECT)
+    game_field = models.ForeignKey(
+        Field,
+        verbose_name="Pista",
+        on_delete=models.PROTECT,
+    )
 
     class Meta:
         verbose_name = "Partit"
